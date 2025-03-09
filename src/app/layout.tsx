88c2b481ec5fd78b125183619a4fe43b93845f36
AbhishekS04 <abhishek23main@gmail.com>
2025-03-09 21:38:20 +0530

    refactor layout structure for improved readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,12 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider>
-          <AuthProvider>
-            <ToastProvider />
-            {children}
-          </AuthProvider>
-        </ThemeProvider>
+      <ThemeProvider>
+        <AuthProvider>
+        <ToastProvider />
+        {children}
+        </AuthProvider>
+      </ThemeProvider>
       </body>
     </html>
   );
